d9cca4d05bb72ed65bd79eacbb7a7b10d08e637a
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2019-09-24 16:25:34 +0200

    adds proper unittests to Julia script
    OK, actually they more closely resemble integration tests, but bear with me. ;)

--- scripts/unittests.jl
+++ scripts/unittests.jl
@@ -1,20 +1,42 @@
 using OMJulia
 using Base.Filesystem
+using Test
 
 moroot = dirname(@__DIR__)
+outdir = "out"
+cd(moroot)
+if !ispath(outdir)
+    mkdir(outdir)
+end
+cd(outdir)
 
 omc = OMJulia.OMCSession()
 try
-    mopath = omc.sendExpression("getModelicaPath()")[3:end-2]
+    mopath = omc.sendExpression("getModelicaPath()")[2:end-2]
     mopath = "$mopath:$(escape_string(moroot))"
     println("Setting MODELICAPATH to ", mopath)
     omc.sendExpression("setModelicaPath(\"$mopath\")")
-    r = omc.sendExpression("loadModel(SHMConduction.Examples.UnidirectionalContractionExample)")
-    println(r)
-    print("Errors: ")
-    println(omc.sendExpression("getErrorString()"))
-    println("loaded")
+    # load Modelica standard library
+    omc.sendExpression("loadModel(Modelica)")
+    @testset "UnidirectionalConductionExample" begin
+        r = omc.sendExpression("loadModel(SHMConduction.Examples.UnidirectionalModularExample)")
+        @test r == "true\n"
+        es = omc.sendExpression("getErrorString()")
+        @test es == "\"\"\n"
+        r = omc.sendExpression("simulate(SHMConduction.Examples.UnidirectionalModularExample, stopTime=50, numberOfIntervals=27500)")
+        es = omc.sendExpression("getErrorString()")
+        @test es == "\"\"\n"
+    end
+    @testset "PVCExample" begin
+        r = omc.sendExpression("loadModel(SHMConduction.Examples.PVCExample)")
+        @test r == "true\n"
+        es = omc.sendExpression("getErrorString()")
+        @test es == "\"\"\n"
+        r = omc.sendExpression("simulate(SHMConduction.Examples.PVCExample, stopTime=50, numberOfIntervals=27500)")
+        es = omc.sendExpression("getErrorString()")
+        @test es == "\"\"\n"
+    end
 finally
+    println("Closing OMC session")
     omc.sendExpression("quit()")
-    println("quit()")
 end
