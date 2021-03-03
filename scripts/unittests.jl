41ed1f410ca8e615b2b124c2c05cf3f3e9b70d33
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-03-03 15:31:50 +0100

    re-adds PVCNoSinus model to unit tests

--- scripts/unittests.jl
+++ scripts/unittests.jl
@@ -1,6 +1,7 @@
 using ModelicaScriptingTools: withOMC, testmodel
 using Base.Filesystem
 using Test
+using OMJulia
 
 projectroot = dirname(@__DIR__)
 
@@ -19,6 +20,12 @@ withOMC(outdir, modeldir) do omc
         end
         @testset "PVCExample" begin
             testmodel(omc, "SHMConduction.Examples.PVCExample"; refdir=refdir)
+            # we cannot change with_sinus using simflags and -override
+            # because it is a structural parameter => create wrapper model
+            OMJulia.sendExpression(omc, "model PVCNoSinus extends SHMConduction.Examples.PVCExample(with_sinus=false); end PVCNoSinus;")
+            testmodel(omc, "PVCNoSinus";  override=Dict(
+                "stopTime"=>50, "numberOfIntervals"=>27500
+            ), refdir=refdir)
         end
     end
 end
